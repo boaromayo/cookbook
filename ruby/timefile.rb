0c8081bb737428405a96db275762105e95eb2567
Nico Poblete <nlypoblet@gmail.com>
2017-06-03 20:08:39 -0700

    Added ruby files

---
+++ ruby/timefile.rb
@@ -0,0 +1,10 @@
+# !/usr/bin/ruby
+
+# timefile.rb
+
+# Print the current time.
+
+# Create Time object.
+t = Time.new
+
+puts "The time now is " + t.asctime + "\n\n" # return a string of the current time.
