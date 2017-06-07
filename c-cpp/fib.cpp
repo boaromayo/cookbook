a7a57fe2dd7b34e6043bc57a2e5a683f582713f6
Nico Poblete <nlypoblet@gmail.com>
2017-06-07 14:55:28 -0700

    Add files via upload

---
+++ c-cpp/fib.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int n) {
+	int x, y;
+	if (n <= 1) {
+		x = fib(n-1);
+		y = fib(n-2);
+		return (x + y);
+	}
+	return n;
+}
+
+int main() {
+	int n = 0;
+	cin >> n;
+	int i = fib(n);
+	
+	cout << i;
+}
