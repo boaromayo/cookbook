37352bbbdcaaad7d2b8f21a04bc0d8a9e4830351
Nico Poblete <nlypoblet@gmail.com>
2017-08-14 16:44:34 -0700

    Fixed initialization bug

--- python/vec3.py
+++ python/vec3.py
@@ -1,10 +1,10 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
+#  !/usr/bin/env python
+#  -*- coding: utf-8 -*-
 #
 #  vec3.py
-#  
-#  Copyright 7/23/2017 boaromayo <boaromayo@retropie>
-#  
+#
+#  copyright 7/23/2017 boaromayo <boaromayo@retropie>
+# 
 #  This will demonstrate classes and other object-oriented topics in Python.
 #
 #  This program is free software; you can redistribute it and/or modify
@@ -29,54 +29,54 @@ class Vec3:
 	def __init__(self,a=0,b=0,c=0):
 		self.a = a
 		self.b = b
-    		self.c = c
+		self.c = c
 		
 	def zero(self):
-		return Vec(0,0,0)
-
+		return Vec3(0,0,0)
+		
 	def one(self):
-		return Vec(1,1,1)
-
+		return Vec3(1,1,1)
+		
 	def isZero(self):
 		return self.a == 0 and self.b == 0 and self.c == 0
-	
+		
 	def isOne(self):
 		return self.a == 1 and self.b == 1 and self.c == 1
-	
+		
 	def isNeg(self):
 		return self.a < 0 and self.b < 0 and self.c < 0
-	
+		
 	def length(self):
 		return sqrt(self.a ** 2 + self.b ** 2 + self.c ** 2)
-
+		
 	def dot(self,v):
 		return self.a * v.a + self.b * v.b + self.c * v.c
-
-  	def cross(self,v):
-    		s1 = self.b * v.c - self.c * v.b
-    		s2 = self.c * v.a - self.a * v.c
-    		s3 = self.a * v.b - self.b * v.a
-    		return Vec(s1,s2,s3)
-    
+		
+	def cross(self,v):
+		s1 = self.b * v.c - self.c * v.b
+		s2 = self.c * v.a - self.a * v.c
+		s3 = self.a * v.b - self.b * v.a
+		return Vec3(s1,s2,s3)
+		
 	def __add__(self,v):
-		return Vec(self.a + v.a, self.b + v.b, self.c + v.c)
-
+		return Vec3(self.a + v.a, self.b + v.b, self.c + v.c)
+		
 	def __sub__(self,v):
-		return Vec(self.a - v.a, self.b - v.b, self.c - v.c)
-
+		return Vec3(self.a - v.a, self.b - v.b, self.c - v.c)
+		
 	def __mul__(self,m):
-		return Vec(m * self.a, m * self.b, m * self.c)
-
+		return Vec3(m * self.a, m * self.b, m * self.c)
+		
 	def __repr__(self):
 		return "Vec a: {} b: {} c: {}".format(self.a, self.b, self.c)
-
+		
 	def __str__(self):
 		return "<{},{},{}>".format(self.a, self.b, self.c)
-
+		
 def main():
-	v = Vec(1,1,2)
+	v = Vec3(1,1,2)
 	print v
 	print v.length()
-
+	
 if __name__ == '__main__':
 	main()
