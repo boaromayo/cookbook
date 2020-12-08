a5165ef00d19e4d15a53c08eb945aa369949f782
Nico Poblete <12805267+boaromayo@users.noreply.github.com>
2020-12-07 16:47:15 -0800

    Remove non-assembler files

--- assembler/snes/color/exec.bat
+++
@@ -1,5 +0,0 @@
-cd "C:\Program Files (x86)\asar"
-asar.exe -v "C:\Users\Owner\Documents\Notepad++ projs\assembler\snes\color\main.asm" "C:\Users\Owner\Documents\Notepad++ projs\assembler\snes\color\color.sfc"
-cd "C:\Users\Owner\Documents\Notepad++ projs\assembler\snes\color"
-
-@pause
